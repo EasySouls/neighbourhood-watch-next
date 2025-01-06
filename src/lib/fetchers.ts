21ec8f40a2f2976f3e24c9cd5f9277b8ed50d343
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-01-06 21:03:14 +0100

    Add axios, swr fetchers and primitive middleware

---
+++ src/lib/fetchers.ts
@@ -0,0 +1,22 @@
+import api from '@/lib/api';
+
+export async function axiosGetFetcher<T>(url: string): Promise<T> {
+  const response = await api.get(url);
+  return response.data;
+}
+
+export async function axiosPostFetcher<T, U>(
+  url: string,
+  { arg }: { arg: U }
+): Promise<T> {
+  const response = await api.post(url, arg);
+  return response.data;
+}
+
+export async function axiosPatchFetcher<T, U>(
+  url: string,
+  { arg }: { arg: U }
+): Promise<T> {
+  const response = await api.patch(url, arg);
+  return response.data;
+}
---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token');
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', request.nextUrl));
+  }
+}
+
+export const config = {
+  mather: ['/profile', '/dashboard'],
+};
