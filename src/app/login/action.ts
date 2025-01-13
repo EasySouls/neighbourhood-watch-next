23431149f86f41cae2a3552eff56ff2db2fe6237
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-01-13 16:51:45 +0100

    Added documentation for login server action and added additional eslint

--- src/app/login/action.ts
+++ src/app/login/action.ts
@@ -5,6 +5,20 @@ import { LoginResponse } from '@/types';
 import { LoginState } from '@/types/actionTypes';
 import { cookies } from 'next/headers';
 
+/**
+ * Handles the login process by sending the provided form data to the login API endpoint.
+ *
+ * @param prevState - The previous state of the login process.
+ * @param formData - The form data containing the user's email and password.
+ * @returns A promise that resolves to the new state of the login process.
+ *
+ * The function performs the following steps:
+ * 1. Extracts the email and password from the form data.
+ * 2. Validates that both email and password are provided.
+ * 3. Sends a POST request to the login API endpoint with the login data.
+ * 4. If the response status is 200 and an access token is received, sets the token in cookies and returns a success state.
+ * 5. Handles errors and returns appropriate error messages for missing data, invalid credentials, or other errors.
+ */
 export async function handleLogin(
   prevState: LoginState,
   formData: FormData
