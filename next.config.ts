e65d7abc63c2fd0e150f47bad4a7b58bc90f225a
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-01-14 20:08:33 +0100

    Enabled experimental ppr feature

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    ppr: 'incremental',
+  },
 };
 
 export default withSentryConfig(nextConfig, {
