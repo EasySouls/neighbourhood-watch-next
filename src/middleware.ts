9f51f8edde08baf94d21cd691d36a8b4511ef631
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-01-24 10:47:56 +0100

    Set Sentry user in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server';
+import api from '@/lib/api';
+import { User } from './types';
+import * as Sentry from '@sentry/nextjs';
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const token = request.cookies.get('token');
   if (!token) {
+    Sentry.setUser(null);
     return NextResponse.redirect(new URL('/login', request.nextUrl));
   }
+  const res = await api.get('/auth/me');
+  if (res.status !== 200) {
+    Sentry.setUser(null);
+    return NextResponse.redirect(new URL('/login', request.nextUrl));
+  }
+  const user = res.data as User;
+  Sentry.setUser({
+    id: user.email,
+    email: user.email,
+    username: `${user.firstName} ${user.lastName}`,
+  });
 }
 
 export const config = {
